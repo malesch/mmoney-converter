fc909de32d42a5c1787c63b136a47490195a0b3c
Marcus Spiegel <malesch@gmail.com>
2018-08-26 16:39:32 +0200

    Refactor generation of Excel sheet
    - Retrofit column definition config
    - First write sheet data and subsequently apply styles

--- dev/src/user.clj
+++ dev/src/user.clj
@@ -2,10 +2,13 @@
   (:require [mmoney-converter.mmoney :as mm]
             [mmoney-converter.excel :as xls]))
 
-; (def data (read-example))
-(defn read-example []
-  (mm/parse-file "example.xml"))
+; (def data (read-example "example.xml"))
+; (def data (read-example "mmoney-export.xml"))
+(defn read-xml [xml-file]
+  (mm/parse-file xml-file))
 
-; (export-data data)
-(defn export-data [data]
-  (xls/export "export.xls" data xls/column-definitions))
+; (export-data "example.xml" "account-mapping.edn" "export.xls")
+; (export-data "mmoney-export.xml" "accounts.edn" "export.xls")
+(defn export-data [xml-file mapping-file out-file]
+  (let [data (read-xml xml-file)]
+    (xls/export out-file data xls/columns mapping-file)))
--- src/mmoney_converter/excel.clj
+++ src/mmoney_converter/excel.clj
@@ -1,75 +1,142 @@
 (ns mmoney-converter.excel
-  (:require [mmoney-converter.mmoney :as mm]
-            [dk.ative.docjure.spreadsheet :as ss]))
+  (:require [clojure.java.io :as io]
+            [clojure.edn :as edn]
+            [dk.ative.docjure.spreadsheet :as ss]
+            [mmoney-converter.mmoney :as mm]))
 
-(def column-definitions
-  [{:key :created
-    :label "Datum"
-    :width (* 20 256)
-    :align :left
-    :date-format "DD.MM.YYYY HH:MM:SS"}
-   {:key :sum
-    :label "Währung"
-    :width (* 10 256)
-    :align :center}
-   {:key :currencyId
-    :label "Betrag"
-    :width (* 10 256)
-    :align :center}
-   {:key :note
-    :label "Beschreibung"
-    :width (* 80 256)
-    :align :left}
-   {:key :categoryId
-    :label "Kategorie"
-    :width (* 40 256) }])
+(def columns
+  [{:column     :account-number
+    :source-key :categoryId
+    :label      "Konto"
+    :styles     {:halign :center}}
+   {:column     :amount
+    :source-key :sum
+    :label      "Betrag"
+    :width      (* 10 256)
+    :styles     {:halign      :center
+                 :data-format "0.00"}}
+   {:column     :date
+    :source-key :created
+    :label      "Datum"
+    :width      (* 20 256)
+    :styles     {:halign      :center
+                 :data-format "dd.mm.yyyy"}}
+   {:column     :currency
+    :source-key :currencyId
+    :label      "Währung"
+    :width      (* 10 256)
+    :styles     {:halign :center}}
+   {:column     :account-name
+    :source-key :categoryId
+    :label      "Kategorie"
+    :width      (* 40 256)
+    :styles     {:halign :left
+                 ; Bug? Halign is not applied.
+                 ; Workaround: Update data-format (or set background color)
+                 :data-format "General"}}
+   {:column     :detail
+    :source-key :note
+    :label      "Beschreibung"
+    :width      (* 80 256)
+    :styles     {:halign :left
+                 ; Bug? Halign is not applied.
+                 ; Workaround: Update data-format (or set background color)
+                 :data-format "General"}}])
 
-(defn write-header [workbook sheet column-names]
-  (-> sheet
-      (ss/add-row! column-names)
-      (ss/set-row-style! (ss/create-cell-style! workbook {:background :grey_25_percent, :font {:bold true}, :halign :center}))))
 
-(defn create-export-sheet! [workbook columns]
-  (let [sheet (-> workbook (ss/sheet-seq) first)]
-    (write-header workbook sheet columns)
-    sheet))
+(defn get-resource-reader [res]
+  (some-> (or (io/resource res)
+              (let [res-file (io/file res)]
+                (when (.exists res-file)
+                  res-file)))
+          (io/reader)))
 
-(defn format-category [lookup c]
-  (some->> c (get lookup) (mm/category-path lookup) (first)))
+(defn read-account-mappings [mapping-file]
+  (if-let [reader (get-resource-reader mapping-file)]
+    (edn/read-string (slurp reader))
+    (throw (ex-info (format "Account mapping file not found: %s" mapping-file) {:resource mapping-file}))))
 
+(defn build-context [data mapping-file]
+  {:category-lookup (mm/category-lookup (:category data))
+   :account-mapping (read-account-mappings mapping-file)})
 
-(defn format-operation-values [cat-lookup op column-defs]
-  (mapv (fn [cdef]
-         (let [v (get op (:key cdef))]
-           (case (:key cdef)
-             :currencyId (when v (name v))
-             :categoryId (format-category cat-lookup v)
-             v)))
-        column-defs))
+(defn select-first-sheet [workbook]
+  (-> workbook (ss/sheet-seq) first))
 
-(defn set-data-column-styles! [sheet column-defs]
+(defn add-export-column-headers! [sheet column-definitions]
+  (let [headers (map :label column-definitions)]
+    (ss/add-row! sheet headers)))
+
+(defmulti format-op-value (fn [_ col-def _] (:column col-def)))
+
+(defmethod format-op-value :account-number [value _ {:keys [category-lookup account-mapping]}]
+  (let [category-path (some->> value (get category-lookup) (mm/category-path category-lookup))
+        leaf-account-name (first category-path)]
+    (or (get account-mapping leaf-account-name)
+        (throw (ex-info (format "Missing account mapping for `%s`" leaf-account-name)
+                        {:account-name leaf-account-name
+                         :category-path category-path})))))
+
+(defmethod format-op-value :account-name [value _ {:keys [category-lookup]}]
+  (let [category-path (some->> value (get category-lookup) (mm/category-path category-lookup))]
+    (first category-path)))
+
+(defmethod format-op-value :default [value _ _]
+  value)
+
+(defn select-operation-values [op column-definitions ctx]
+  (map (fn [cdef]
+         (-> op
+             (get (:source-key cdef))
+             (format-op-value cdef ctx)))
+       column-definitions))
+
+(defn add-export-data! [sheet data column-definitions xfs]
+  (doseq [op (:operation data)]
+    (as-> op $
+          (select-operation-values $ column-definitions xfs)
+          (ss/add-row! sheet $))))
+
+(defn write-export-sheet [sheet data column-definitions xfs]
+  (doto sheet
+    (add-export-column-headers! column-definitions)
+    (add-export-data! data column-definitions xfs)))
+
+(defn set-column-widths! [sheet column-definitions]
+  (doseq [[idx {:keys [width]}] (map-indexed vector column-definitions)]
+    (when width
+      (.setColumnWidth sheet idx width))))
+
+(defn style-column-header! [sheet]
+  (let [workbook (.getWorkbook sheet)
+        header (-> sheet (ss/row-seq) first)]
+    (ss/set-row-style! header (ss/create-cell-style! workbook {:background :grey_25_percent, :font {:bold true}, :halign :center}))))
+
+(defn style-data-rows!
+  "Iterate over all data cells and set styles individually because of problems setting styles on columns
+  (using SXSSFSheet#setDefaultColumnStyle) and to skip manipulating the header row."
+  [sheet column-definitions]
   (let [workbook (.getWorkbook sheet)]
-    (doall (map-indexed
-             (fn [idx {:keys [width align]}]
-               (when align
-                 (.setDefaultColumnStyle sheet idx (ss/create-cell-style! workbook {:halign align})))
-               (when width
-                 (.setColumnWidth sheet idx width)))
-             column-defs)))
-  sheet)
+    (doseq [row (-> sheet (ss/row-seq) (rest))
+            :let [row-cells (ss/cell-seq row)]]
+      (doseq [[idx cdef] (map-indexed vector column-definitions)]
+        (let [cell (nth row-cells idx)
+              cell-style (:styles cdef)]
+          (when-not (empty? cell-style)
+            (ss/set-cell-style! cell (ss/create-cell-style! workbook cell-style))))))))
 
-(defn add-data-rows [sheet {:keys [category operation]} column-defs]
-  (let [category-lookup (mm/category-lookup category)]
-    (doseq [op operation
-            :let [values (format-operation-values category-lookup op column-defs)]]
-      (-> sheet
-          (ss/add-row! values)
-          #_(set-data-cell-styles! column-defs)))))
+(defn style-export-sheet [sheet column-definitions]
+  (doto sheet
+    (set-column-widths! column-definitions)
+    (style-column-header!)
+    (style-data-rows! column-definitions)))
 
-(defn export [fout data column-defs]
-  (let [wb (ss/create-workbook "MMoney Export" nil)
-        sheet (-> (create-export-sheet! wb (map :label column-defs))
-                  (set-data-column-styles! column-defs))]
-    (add-data-rows sheet data column-defs)
-    (ss/save-workbook! fout wb)
+(defn export [fout data column-definitions account-mapping-file]
+  (let [workbook (ss/create-workbook "mMoney Export" nil)
+        ctx (build-context data account-mapping-file)]
+    (-> workbook
+        (select-first-sheet)
+        (write-export-sheet data column-definitions ctx)
+        (style-export-sheet column-definitions))
+    (ss/save-workbook! fout workbook)
     :OK))
