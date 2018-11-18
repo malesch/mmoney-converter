22677b32abaf3053e62c9462144729d9187553cb
Marcus Spiegel <malesch@gmail.com>
2018-11-18 12:05:59 +0100

    Fix wrong date selector in configuration file

--- dev/src/user.clj
+++ dev/src/user.clj
@@ -1,14 +1,15 @@
 (ns user
-  (:require [mmoney-converter.mmoney :as mm]
+  (:require [mmoney-converter.core :as core]
+            [mmoney-converter.mmoney :as mm]
             [mmoney-converter.excel :as xls]))
 
-; (def data (read-example "example.xml"))
-; (def data (read-example "mmoney-export.xml"))
+; (def data (read-xml "example.xml"))
+; (def data (read-xml "mmoney-export.xml"))
 (defn read-xml [xml-file]
   (mm/parse-file xml-file))
 
-; (export-data "example.xml" "account-mapping.edn" "export.xls")
-; (export-data "mmoney-export.xml" "kontoplan.txt" "export.xls")
-#_(defn export-data [xml-file mapping-file out-file]
-  (let [data (read-xml xml-file)]
-    (xls/export data xls/columns mapping-file out-file)))
+; (export-data "config.edn" "example.xml" "export.xls")
+(defn export-data [config xml-file out-file]
+  (let [config (core/read-configuration config)
+        data (read-xml xml-file)]
+    (xls/export data config out-file)))
